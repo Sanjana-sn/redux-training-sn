47bc27cae6178bbb6da4298a9c1d23e0f1b47b5b
sanjana <sanjana.sn7@gmail.com>
2020-10-22 16:17:38 +0530

    redux 1st task

--- react-redux/src/index.js
+++ react-redux/src/index.js
@@ -7,9 +7,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && 
-  window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducer
 )
 
 render(
